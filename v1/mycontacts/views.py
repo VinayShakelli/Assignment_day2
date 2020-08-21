a944407eeebdbd354523b04128887b7f6716160b
Vinay Shakelli <vinay.shakelli@gmail.com>
2020-08-21 11:00:11 +0530

    second assignment

--- v1/mycontacts/views.py
+++ v1/mycontacts/views.py
@@ -8,4 +8,5 @@ def name(requests):
     return HttpResponse('<h1>My name is Vinay Shakelli</h1>')
 def number(requests):
     return HttpResponse('<h1> My number is 9888545690</h1>')
-
+def whoami(requests):
+    return HttpResponse('<h1> I am Vinay, my mail is vinay@gmail.com</h1>')
